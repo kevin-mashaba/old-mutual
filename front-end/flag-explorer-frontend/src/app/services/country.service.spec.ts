1e340668b2221ba309246a452dd4f5622d3c33b2
kevin-mashaba <kevinkmashaba@gmail.com>
2025-05-11 15:10:43 +0200

    adding test for getAllCountries

--- front-end/flag-explorer-frontend/src/app/services/country.service.spec.ts
+++ front-end/flag-explorer-frontend/src/app/services/country.service.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CountryService } from './country.service';
+import { HttpTestingController } from '@angular/common/http/testing';
+import { Country } from '../model/Country';
 
-describe('CountryService', () => {
   let service: CountryService;
+  let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CountryService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+
+  describe('CountryService', () => {
+    let service: CountryService;
+    let httpMock: HttpTestingController;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [CountryService],
+      });
+  
+      service = TestBed.inject(CountryService);
+      httpMock = TestBed.inject(HttpTestingController);
+    });
+  
+    afterEach(() => {
+      httpMock.verify();
+    });
+  
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  
+    it('should fetch all countries', () => {
+      const dummyCountries: Country[] = [
+        {
+          name: 'Kenya',
+          capital: 'Nairobi',
+          population: 53000000,
+          code: 'KE',
+          flagUrl: 'https://flagcdn.com/ke.svg',
+        },
+      ];
+  
+      service.getAllCountries().subscribe((countries) => {
+        expect(countries.length).toBe(1);
+        expect(countries).toEqual(dummyCountries);
+      });
+  
+      const req = httpMock.expectOne('http://localhost:8081/countries');
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyCountries);
+    });
   });
-});
+  
+  
+
